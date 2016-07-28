806cd5eaf9e80d2572e019eb62a9068e2ffbb9f2
moc <758548361@qq.com>
2016-07-28 18:16:46 +0800

    添加组件

--- src/app/application/application-list.component.ts
+++ src/app/application/application-list.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
+import {HeaderComponent} from "../inc/header.component";
 
 @Component({
     template: `
+    <my-header></my-header>
     <h2>APPs</h2>
     <p>Please show apps here</p>
     <a [routerLink]="['/apps/1']">app detail</a>
     `,
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, HeaderComponent]
 })
 export class ApplicationListComponent {
 }
