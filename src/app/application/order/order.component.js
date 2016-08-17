be2b313dcb7409baa527ab9cc1e2d032ce0afe25
moc <758548361@qq.com>
2016-08-17 13:57:51 +0800

    日期控件中文

--- src/app/application/order/order.component.js
+++ src/app/application/order/order.component.js
@@ -55,6 +55,13 @@ var OrderComponent = (function () {
     OrderComponent.prototype.initDatePicker = function () {
         var _this = this;
         $('.daterange').daterangepicker({
+            locale: {
+                applyLabel: '确定',
+                cancelLabel: '取消',
+                format: 'YYYY/MM/DD',
+                daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
+                monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            },
             maxDate: moment()
         }, function (start, end) { return _this.cb(start, end); });
         this.cb(moment().subtract(6, 'days'), moment());
--- src/app/application/order/order.component.ts
+++ src/app/application/order/order.component.ts
@@ -75,6 +75,13 @@ export class OrderComponent implements OnInit, OnDestroy {
 
     initDatePicker() {
         $('.daterange').daterangepicker({
+            locale: {
+                applyLabel: '确定',
+                cancelLabel: '取消',
+                format: 'YYYY/MM/DD',
+                daysOfWeek: ['日','一','二','三','四','五','六'],
+                monthNames: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+            },
             maxDate: moment()
         }, (start: Moment, end: Moment) => this.cb(start, end));
         this.cb(moment().subtract(6, 'days'), moment());
