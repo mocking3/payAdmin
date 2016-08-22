cab24924535383946c7a130f2e659e8115df3ed4
moc <758548361@qq.com>
2016-08-22 09:41:07 +0800

    日期默认值

--- src/app/+applications/application/analysis/order-analysis/order-analysis.component.ts
+++ src/app/+applications/application/analysis/order-analysis/order-analysis.component.ts
@@ -123,6 +123,8 @@ export class OrderAnalysisComponent implements OnInit, OnDestroy {
                 daysOfWeek: ['日','一','二','三','四','五','六'],
                 monthNames: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
             },
+            startDate: moment().subtract(6, 'days'),
+            endDate: moment(),
             maxDate: moment()
         }, (start: Moment, end: Moment) => this.cb(start, end));
     }
--- src/app/+applications/application/orders/order-list.component.ts
+++ src/app/+applications/application/orders/order-list.component.ts
@@ -83,6 +83,8 @@ export class OrderComponent implements OnInit, OnDestroy {
                 daysOfWeek: ['日','一','二','三','四','五','六'],
                 monthNames: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
             },
+            startDate: moment().subtract(6, 'days'),
+            endDate: moment(),
             maxDate: moment()
         }, (start: Moment, end: Moment) => this.cb(start, end));
         this.cb(moment().subtract(6, 'days'), moment());
